b9725792ccc0005f5d0ccd1009a7f62b91ab4642
Alexandr <sashkhud7@mail.ru>
2026-03-17 10:52:18 +0700

    for vuz

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import Birthday
 from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
+
 
 BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
@@ -25,6 +27,13 @@ class BirthdayForm(forms.ModelForm):
         first_name = self.cleaned_data['first_name']
         last_name = self.cleaned_data['last_name']
         if f'{first_name} {last_name}' in BEATLES:
+            send_mail(
+                subject='Anotehr Beatles member',
+                message=f'{first_name} {last_name} пытался опубликовать запись!',
+                from_email='birthday_form@acme.not',
+                recipient_list=['admin@acme.not'],
+                fail_silently=True,
+            )
             raise ValidationError(
                 'Мы тоже любим Битлз, но введите, пождалуйста, настоящее имя!'
             )
